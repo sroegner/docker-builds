2c3655551eebd9cc56147f7931ee4bdc641a4b8f
Steffen Roegner <steffen@sroegner.org>
2014-03-18 07:50:25 -0400

    added default accumulo config

---
+++ accumulo/accumulo.conf/accumulo-env.sh
@@ -0,0 +1,64 @@
+#! /usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###
+### Configure these environment variables to point to your local installations.
+###
+### The functional tests require conditional values, so keep this style:
+###
+### test -z "$JAVA_HOME" && export JAVA_HOME=/usr/local/lib/jdk-1.6.0
+###
+###
+### Note that the -Xmx -Xms settings below require substantial free memory: 
+### you may want to use smaller values, especially when running everything
+### on a single machine.
+###
+
+export ACCUMULO_HOME=/usr/lib/accumulo
+
+if [ -z "$HADOOP_HOME" ]
+then
+	test -z "$HADOOP_PREFIX"      && export HADOOP_PREFIX=/usr/lib/hadoop
+else
+   HADOOP_PREFIX="$HADOOP_HOME"
+   unset HADOOP_HOME
+fi
+
+test -z "$HADOOP_CONF_DIR"       && export HADOOP_CONF_DIR=/etc/hadoop/conf
+test -z "$JAVA_HOME"             && export JAVA_HOME=/usr/lib/java
+test -z "$ZOOKEEPER_HOME"        && export ZOOKEEPER_HOME=/usr/lib/zookeeper
+test -z "$ACCUMULO_LOG_DIR"      && export ACCUMULO_LOG_DIR=/var/log/accumulo
+if [ -f /etc/accumulo/conf/accumulo.policy ]
+then
+	POLICY="-Djava.security.manager -Djava.security.policy=/etc/accumulo/conf/accumulo.policy"
+fi
+
+TSERVER_DBG=""
+MASTER_DBG=""
+MONITOR_DBG=""
+GC_DBG=""
+
+export JMX_OPTS=" -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=127.0.0.1"
+MASTER_OOM_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$ACCUMULO_LOG_DIR/master.hpro"
+TSERVER_OOM_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$ACCUMULO_LOG_DIR/tserver.hpro"
+test -z "$ACCUMULO_TSERVER_OPTS" && export ACCUMULO_TSERVER_OPTS="$TSERVER_DBG $TSERVER_OOM_OPTS $JMX_OPTS -Dcom.sun.management.jmxremote.port=26051 ${POLICY} -Xmx3072m -Xms128m "
+test -z "$ACCUMULO_MASTER_OPTS"  && export ACCUMULO_MASTER_OPTS="$MASTER_DBG $MASTER_OOM_OPTS $JMX_OPTS -Dcom.sun.management.jmxremote.port=26052 ${POLICY} -Xmx3072m -Xms128m"
+test -z "$ACCUMULO_MONITOR_OPTS" && export ACCUMULO_MONITOR_OPTS="$MONITOR_DBG $JMX_OPTS -Dcom.sun.management.jmxremote.port=26053 ${POLICY} -Xmx1536m -Xms64m"
+test -z "$ACCUMULO_GC_OPTS"      && export ACCUMULO_GC_OPTS="$GC_DBG $JMX_OPTS -Dcom.sun.management.jmxremote.port=26054 -Xmx1536m -Xms64m"
+test -z "$ACCUMULO_GENERAL_OPTS" && export ACCUMULO_GENERAL_OPTS="-XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75"
+test -z "$ACCUMULO_OTHER_OPTS"   && export ACCUMULO_OTHER_OPTS="-Xmx3072m -Xms64m"
+export ACCUMULO_LOG_HOST=`(grep -v '^#' /etc/accumulo/conf/monitor ; echo localhost ) 2>/dev/null | head -1`
