968cc137156640a2c58316a462930ab8c26276d0
Steffen Roegner <steffen@sroegner.org>
2014-07-07 17:24:59 -0400

    fixed accumulo java references, fixed start order in supervisor, updated accumulo Makefile

--- accumulo/init_accumulo.sh
+++ accumulo/init_accumulo.sh
@@ -2,5 +2,5 @@ su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir /accumulo'
 su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir -p /user/accumulo'
 su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /accumulo'
 su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /user/accumulo'
-su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password secret'
+su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password DEFAULT'
 cp -vu /docker/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
