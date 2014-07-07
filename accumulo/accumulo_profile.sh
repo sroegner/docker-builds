6663c2da1611d91169a0a76dc1b66d73b07f9d6a
David Medinets <david.medinets@gmail.com>
2014-07-07 23:41:00 +0000

    Update PATH to include hadoop and accumulo.

--- accumulo/accumulo_profile.sh
+++ accumulo/accumulo_profile.sh
@@ -1,2 +1,15 @@
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk.x86_64
 
+pathmunge () {
+        if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then
+           if [ "$2" = "after" ] ; then
+              PATH=$PATH:$1
+           else
+              PATH=$1:$PATH
+           fi
+        fi
+}
+
+pathmunge /usr/lib/hadoop/bin
+pathmunge /usr/lib/accumulo/bin
+
