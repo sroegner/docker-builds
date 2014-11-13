d8df94d0de2f0096e4a59891ecca16127ffc843a
Steffen Roegner <steffen.roegner@gmail.com>
2014-11-13 15:28:05 -0500

    move towards cluster-readiness, Accumulo version now 1.6.1

--- accumulo/init_accumulo.sh
+++ accumulo/init_accumulo.sh
@@ -1,6 +1,46 @@
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir /accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -mkdir -p /user/accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /accumulo'
-su - hdfs -c '/usr/lib/hadoop/bin/hdfs dfs -chown accumulo:accumulo /user/accumulo'
+#!/bin/bash
+
+if [ -f /etc/hadoop/conf/core-site.xml ]
+then
+  namenode_host=$(grep 8020 /etc/hadoop/conf/core-site.xml|sed 's%.*hdfs://%%'|awk -F':' '{print $1}')
+fi
+
+if [ "${namenode_host}" != "$(hostname)" ]
+then
+  supervisorctl stop all
+  /etc/hadoop/conf.templates/format-namenode.sh
+fi
+
+supervisorctl start all
+
+su - hdfs << EOF
+echo
+echo "Preparing for Accumulo initialization"
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -mkdir /accumulo /user/accumulo
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -chown accumulo:accumulo /accumulo /user/accumulo
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -ls /
+EOF
+
+NAMENODE_HOST=$(grep master /etc/hosts|tail -1|awk '{print $2}')
+HOSTNAME=$(hostname)
+IP=$(hostname -I)
+CONF=/var/lib/accumulo/conf
+
+rm -rf $CONF
+cp -r ${CONF}.templates $CONF
+
+if [ "NAMENODE_HOST" != "" ]
+then
+  sed -i "s/{{ master_hostname }}/${NAMENODE_HOST}/g" ${CONF}/*
+fi
+
+for f in masters monitor gc tracer slaves
+do
+  echo $HOSTNAME > ${CONF}/$f
+done
+
+echo
+echo "Initializing Accumulo"
 su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password DEFAULT'
-cp -vu /docker/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
+mv /var/lib/accumulo/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
+
