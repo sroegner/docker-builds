343cc748204972b8b1f57f898cd4e0753701aaa6
Steffen Roegner <steffen.roegner@gmail.com>
2014-11-30 12:37:52 -0500

    fixed trace password, fixed bind address, added proxy, separated tserver in supervisor

--- accumulo/init_accumulo.sh
+++ accumulo/init_accumulo.sh
@@ -16,9 +16,9 @@ supervisorctl start all
 su - hdfs << EOF
 echo
 echo "Preparing for Accumulo initialization"
-JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -mkdir /accumulo /user/accumulo
-JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -chown accumulo:accumulo /accumulo /user/accumulo
-JAVA_HOME=/usr HADOOP_ROOT_LOGGER="WARN,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -ls /
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="ERROR,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -mkdir /accumulo /user/accumulo
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="ERROR,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -chown accumulo:accumulo /accumulo /user/accumulo
+JAVA_HOME=/usr HADOOP_ROOT_LOGGER="ERROR,console" HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec /usr/lib/hadoop-hdfs/bin/hdfs dfs -ls /
 EOF
 
 NAMENODE_HOST=$(grep master /etc/hosts|tail -1|awk '{print $2}')
@@ -26,6 +26,8 @@ HOSTNAME=$(hostname)
 IP=$(hostname -I)
 CONF=/var/lib/accumulo/conf
 
+echo "The namenode is ${NAMENODE_HOST}"
+echo "This host is $HOSTNAME"
 rm -rf $CONF
 cp -r ${CONF}.templates $CONF
 
@@ -34,13 +36,14 @@ then
   sed -i "s/{{ master_hostname }}/${NAMENODE_HOST}/g" ${CONF}/*
 fi
 
-for f in masters monitor gc tracer slaves
+for f in masters monitor gc tracers slaves
 do
   echo $HOSTNAME > ${CONF}/$f
 done
 
 echo
 echo "Initializing Accumulo"
-su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password DEFAULT'
+su - accumulo -c '/usr/lib/accumulo/bin/accumulo init --instance-name accumulo --password DOCKERDEFAULT'
 mv /var/lib/accumulo/supervisord-accumulo.conf /etc/supervisor/conf.d/accumulo.conf
+mv /var/lib/accumulo/supervisord-accumulo-tserver.conf /etc/supervisor/conf.d/accumulo-tserver.conf
 
