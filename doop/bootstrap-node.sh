c8ee74e88767cab5120bb7ddccb416b34ad3c454
Steffen Roegner <steffen.roegner@gmail.com>
2015-05-07 13:25:41 -0400

    add bootstrap logging, add more accumulo supervisor conf

--- doop/bootstrap-node.sh
+++ doop/bootstrap-node.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-#  supervisorctl start doop-datanode:\*
+# a generic bootstrapper to put service checks in place for consul
+# and start services via supervisor
+# The supervisor config has to be part of the image, the list of service
+# is declared as comma-separated list 
+#   docker run ... -e SVCLIST=aservice,bservice ...
+
 if [ "$SVCLIST" != "" ]
 then
   services=(${SVCLIST//,/ })
   for svc in "${services[@]}"
   do
+    echo "[$(date)] starting $svc"
     svccheck=/etc/consul/services/${svc}_service.json
-    [ -f $svccheck ] && cp $svccheck /etc/consul
+    if [ -f $svccheck ]
+    then
+      echo "[$(date)] Add consul service check for $svc"
+      cp $svccheck /etc/consul
+    else
+      echo "[$(date)] No consul service check for $svc"
+    fi
     supervisorctl start $svc
+    sleep 1
+    supervisorctl status $svc
   done
 fi
 
