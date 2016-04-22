a6f0c53182ad2ebfa1b181f91efdd29626c30897
Steffen Roegner <steffen.roegner@gmail.com>
2016-04-22 11:40:39 -0400

    fix start script

--- accumulo/start.sh
+++ accumulo/start.sh
@@ -1,87 +1,58 @@
 tag=docker.io/sroegner/accumulo
-container_name=accumulo-cluster-0
-# the link to the leader allows the consul agents inside the containers
-# to communicate - every node has the consul web ui on port 8500
-link_arg="--link=consul-leader:consul-leader"
+container_name=accumulo-cluster
+accumulo_net=accumulo-docker-local
 
 if [ "$1" = "-clean" ]
 then
   echo cleaning up as requested
   docker rm -f consul-leader
-  for c in namenode zk0 tserver0 tserver1 tserver2 master proxy
+  for c in namenode zookeeper tserver0 tserver1 tserver2 master proxy
   do
     docker rm -f ${container_name}-${c}
   done
 fi
 
-# start a single (not recommended) consul leader container
-echo start consul leader
-docker run -d --name=consul-leader \
-              --hostname=consul.docker.local ${tag} \
-              /usr/sbin/consul agent -server -bootstrap-expect=1 \
-              -data-dir=/var/lib/consul/data -config-dir=/etc/consul-leader
+run_container() {
+  fqdn="${1}.docker.local"
+  /usr/bin/docker run -d --net=${accumulo_net} \
+                         --hostname=${fqdn} --net-alias=${fqdn} \
+                         -e="SVCLIST=${2}" \
+                         --name=${container_name}-${1} ${tag} \
+                         /usr/bin/supervisord -n
+}
 
-# need the ip of the consul server as the dns address used by all others
-dns_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' consul-leader)
+# the cluster requires DNS to work which we use a custom docker network
+# with its embedded DNS server for
+$(/usr/bin/docker network inspect ${accumulo_net} &>/dev/null) || {
+  # TODO: would be nice to check that the local docker client is new enough for this
+  /usr/bin/docker network create -d bridge --subnet=172.25.10.0/24 $accumulo_net
+}
 
-echo start hdfs
-# start the namenode, secondarynamenode and a datanode process
-# the datanode process is a compromise to allow accumulo init to create dfs objects
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=namenode.docker.local ${link_arg} \
-              -e="SVCLIST=namenode,secondarynamenode,datanode" \
-              --name=${container_name}-namenode ${tag} /usr/bin/supervisord -n
-
-echo start a zookeeper container before init
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=zookeeper.docker.local ${link_arg} \
-              -e="SVCLIST=zookeeper" \
-              --name=${container_name}-zk0  ${tag} /usr/bin/supervisord -n
-
-echo hdfs needs a moment to become available
-sleep 20
-# now we can go back to the namenode and run accumulo init
-# this will create some state in dfs and zookeeper
+echo "Welcome to Accumulo on Docker!"
+echo
+echo "starting hdfs"
+run_container namenode 'namenode,datanode'
+echo "start zookeeper"
+run_container zookeeper 'zookeeper'
+echo "hdfs needs a moment to become available"
+sleep 10
+echo "initializing Accumulo instance"
 docker exec ${container_name}-namenode /usr/lib/accumulo/bin/init_accumulo.sh
-
-echo start tservers
-# ready to start some tablet servers
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=tserver0.docker.local ${link_arg} \
-              -e="SVCLIST=datanode,accumulo-tserver" \
-              --name=${container_name}-tserver0 ${tag} /usr/bin/supervisord -n
-
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=tserver1.docker.local ${link_arg} \
-              -e="SVCLIST=datanode,accumulo-tserver" \
-              --name=${container_name}-tserver1 ${tag} /usr/bin/supervisord -n
-
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=tserver2.docker.local ${link_arg} \
-              -e="SVCLIST=datanode,accumulo-tserver" \
-              --name=${container_name}-tserver2 ${tag} /usr/bin/supervisord -n
-
-echo start accumulo master
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=master.docker.local ${link_arg} \
-              -e="SVCLIST=accumulo-master,accumulo-monitor,accumulo-gc,accumulo-tracer" \
-              --name=${container_name}-master ${tag} /usr/bin/supervisord -n
-
-echo start accumulo proxy
-docker run -d --dns=${dns_ip} --dns-search=docker.local \
-              --hostname=proxy.docker.local ${link_arg} \
-              -e="SVCLIST=accumulo-proxy" \
-              --name=${container_name}-proxy ${tag} /usr/bin/supervisord -n
-
-sleep 20
-# creating an accumulo user should now work
-
+echo "start tservers"
+run_container tserver0 'datanode,accumulo-tserver'
+run_container tserver1 'datanode,accumulo-tserver'
+run_container tserver2 'datanode,accumulo-tserver'
+echo "start accumulo master"
+run_container master 'accumulo-master,accumulo-monitor'
+echo "start accumulo proxy, gc and tracer"
+run_container proxy 'accumulo-tracer,accumulo-gc,accumulo-proxy'
+echo "wait for accumulo services to start"
+sleep 10
 echo create user alfred with password batman
 docker exec ${container_name}-tserver0 /tmp/add_user.sh alfred batman
 echo
-master_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${container_name}-master)
+
+master_ip=$(docker inspect --format '{{(index .NetworkSettings.Networks "accumulo-docker-local").IPAddress}}' ${container_name}-master)
 echo "Accumulo Monitor is at http://${master_ip}:50095"
-echo "Check cluster service health at http://${dns_ip}:8500"
-echo
-echo -e "Login to accumulo with \n\t docker exec -it ${container_name}-tserver0 su - accumulo -c \"/usr/lib/accumulo/bin/accumulo shell -u alfred -p batman\""
+echo -e "Login to accumulo with \n\t docker exec -u accumulo -it ${container_name}-tserver0 /usr/lib/accumulo/bin/accumulo shell -u alfred -p batman"
 echo
